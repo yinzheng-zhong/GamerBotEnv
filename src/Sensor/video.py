92519a3fee20e1cd6948cd24f9a7fb18065bbc0c
zxhaxdr <12530685+yinzheng-zhong@users.noreply.github.com>
2022-05-09 07:58:33 +0100

    Checkpoint

--- src/Sensor/video.py
+++ src/Sensor/video.py
@@ -7,12 +7,14 @@ import collections
 from multiprocessing import Process
 
 import pyautogui
-import multiprocessing
+import threading
 import time
 import numpy as np
 
 from src.Helper.config_reader import NN, Capturing
 import src.Utils.image as image_utils
+
+FRAME_TIME_QUEUE_SIZE = 10
 
 
 class Capture:
@@ -22,37 +24,51 @@ class Capture:
         self.resolution = Capturing.get_resolution()
 
         self.screenshot_list = collections.deque(maxlen=self.max_screenshots)
+        self.timestamps = collections.deque(maxlen=FRAME_TIME_QUEUE_SIZE)
 
         # initialize the deque
         self.screenshot_list.extend(
-            [np.zeros((self.resolution[0], self.resolution[1], 3))] * self.max_screenshots
+            [np.zeros((self.resolution[0], self.resolution[1], 3), dtype=np.uint8)] * self.max_screenshots
         )
+        self.timestamps.extend(range(FRAME_TIME_QUEUE_SIZE))
 
         self.run()
 
     def capture_latest(self):
-        image = pyautogui.screenshot()
-        image = np.array(image).astype(np.uint8)
-
-        if any(self.resolution):
-            return image_utils.scale_image(image, self.resolution)
-        else:
-            return image
-
-    def capture_historical(self):
         while True:
-            image = self.capture_latest()
-            self.screenshot_list.append(image)
+            if time.time() - self.timestamps[-1] < 1 / self.frame_rate:
+                time.sleep(0.01)
+                continue
 
-            time.sleep(1 / self.frame_rate)
+            image = pyautogui.screenshot()
+            image = np.array(image).astype(np.uint8)
+
+            if any(self.resolution):
+                image = image_utils.scale_image(image, self.resolution)
+            else:
+                image = image
+
+            # if time.time() - self.last_captured > 1 / self.frame_rate:
+            #     self.screenshot_list.append(image)
+            #     self.last_captured = time.time()
+
+            self.screenshot_list.append(image)
+            self.timestamps.append(time.time())
+
+    def print_frame_rate(self):
+        while True:
+            mean_time = (self.timestamps[-1] - self.timestamps[0]) / (FRAME_TIME_QUEUE_SIZE - 1)
+            print("Actual frame rate: {}".format(1 / mean_time))
+            time.sleep(1)
 
     def run(self):
-        #self.capture()
-
-        capture_process = Process(target=self.capture_historical)
+        capture_process = threading.Thread(target=self.capture_latest)
         capture_process.start()
 
-        print("Capture process started")
+        frame_rate_process = threading.Thread(target=self.print_frame_rate)
+        frame_rate_process.start()
+
+        print("Capture thread started")
         #capture_process.join()
 
     def get_screenshot_series(self):
