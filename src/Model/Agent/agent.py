Fixed the tensor reference being copied and lost gradient function

src/Model/Agent/agent.py
@@ -130,7 +130,10 @@ class Agent:
         return self._input_queue.get()
 
     def update_epsilon(self):
-        self.epsilon *= self.epsilon_decay if self.epsilon > self.epsilon_min else self.epsilon_min
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+        else:
+            self.epsilon = self.epsilon_min
 
     def train(self):
         batch = self.replay_memory.sample()
@@ -156,7 +159,7 @@ class Agent:
 
             # Update Q value for given state
             #  for predicted not human: current_qs = current_q_array[0][index]
-            current_qs = current_q_array[index]
+            current_qs = current_q_array[index].clone()
             current_qs[torch.argmax(action)] = new_q
 
             y.append(current_qs)
@@ -175,8 +178,9 @@ class Agent:
         print('Agent is running')
 
         try:
+            self.main_model.load_state_dict(torch.load(self.weight_file))
             self.target_model.load_state_dict(torch.load(self.weight_file))
-        except _pickle.UnpicklingError:
+        except (_pickle.UnpicklingError, RuntimeError):
             print('Model params have changed')
         except FileNotFoundError:
             print('Model weight file not found')
@@ -202,7 +206,7 @@ class Agent:
 
                 predicted_action = self.key_mapping.get_key_from_on_hot_mapping(numpy_action)
 
-                print('\npredicted_action: {}'.format(predicted_action))
+                print('\npredicted_action: {}'.format(numpy_action))
 
                 new_state, reward = self.action(numpy_action)
                 new_state = new_state[:3]  # remove the feedback

src/Model/NN/nn_pytorch.py
@@ -11,8 +11,20 @@ from src.Helper.configs import Keys as key_config
 
 class CNN(nn.Module):
     def __init__(self):
-        super(CNN, self).__init__()
+        super().__init__()
         self.key_output_size = key_config.get_key_mapping_size()
+
+        self.input_screen_dim = (
+            1,
+            nn_config.get_screenshot_input_dim()[1],
+            nn_config.get_screenshot_input_dim()[0]
+        )  # (720, 1280)
+
+        self.input_sound_dim = (
+            1,
+            constance.NN_SOUND_SPECT_INPUT_DIM[1],
+            constance.NN_SOUND_SPECT_INPUT_DIM[0]
+        )
 
         self.screen = nn.Sequential(
             nn.Conv2d(1, 16, (3, 3), stride=1),
@@ -37,8 +49,10 @@ class CNN(nn.Module):
             nn.Flatten(),
         )
 
+        self.fc_input_size = self.calc_fc_input_size()
+
         self.combined = nn.Sequential(
-            nn.Linear(38336, 64),
+            nn.Linear(sum(self.fc_input_size), 64),
             nn.ReLU(inplace=True),
             nn.Linear(64, 24),
             nn.ReLU(inplace=True),
@@ -46,18 +60,20 @@ class CNN(nn.Module):
         )
 
     def forward(self, screen, sound_l, sound_r):
-        screen = self.screen(screen).view(-1, 22848)
-        sound_l = self.sound(sound_l).view(-1, 7744)
-        sound_r = self.sound(sound_r).view(-1, 7744)
+        screen = self.screen(screen).view(-1, self.fc_input_size[0])
+        sound_l = self.sound(sound_l).view(-1, self.fc_input_size[1])
+        sound_r = self.sound(sound_r).view(-1, self.fc_input_size[2])
         combined = torch.cat((screen, sound_l, sound_r), 1)
         output = self.combined(combined)
         return output
 
+    def calc_fc_input_size(self):
+        screen = torch.zeros(self.input_screen_dim).unsqueeze(0)
+        screen = self.screen(screen)
+        screen = screen.shape[1]
 
-class LSTM(nn.Module):
-    def __init__(self):
-        super(LSTM, self).__init__()
+        sound = torch.zeros(self.input_sound_dim).unsqueeze(0)
+        sound = self.sound(sound)
+        sound = sound.shape[1]
 
-        self.key_output_size = key_config.get_key_mapping_size()
-
-        self.screen = nn.Sequential()
+        return screen, sound, sound